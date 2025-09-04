f15b702c5331b7288ae899bd53009c2833ef6c7c
alonegamer314 <alonegamer9696@gmail.com>
2025-09-05 01:58:38 +0530

    Update signin.js

--- js/signin.js
+++ js/signin.js
@@ -1,1 +1,22 @@
 
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const auth = getAuth();
+const signinForm = document.getElementById('signinForm');
+
+if (signinForm) {
+  signinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = signinForm['signinEmail'].value;
+    const password = signinForm['signinPassword'].value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        alert(`Signin successful! Welcome back`);
+        window.location.href = "index.html"; // redirect to homepage
+      })
+      .catch((error) => {
+        alert(`Error: ${error.message}`);
+      });
+  });
+}
